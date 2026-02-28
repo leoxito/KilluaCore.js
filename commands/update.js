31093fcbd8e6cd80d83c602e3f5586308495388e
﹫𝐄𝐯𝐨𝐂𝐨𝐫𝐞.𝐣𝐬 <erenxz01@gmail.com>
2026-02-28 19:42:12 -0400

    Actualizar update.js

--- commands/update.js
+++ commands/update.js
@@ -1,5 +1,5 @@
 case 'update': case 'up':
-                        if (!global.owner.some(o => sender.includes(o[0]))) return reply(`Comando no encontrado: *${command}*\n\nUsa *${usedPrefix}help* para ver los comandos disponibles`)
+                        if (!global.owner.some(o => sender.includes(o[0]))) return reply(`*🌴 Este Comandos No Esta Disponible En Mi Base De Datos*: *${command}*\n\n> _Te Recomiendo Usar *${usedPrefix}help* para ver los comandos disponibles._`)
                         await reply('Reiniciando sistema...')
                         process.exit(0)
                         break
