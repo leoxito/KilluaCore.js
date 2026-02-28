e27c21da2572bb3dc0205945084721b84c17b76c
﹫𝐄𝐯𝐨𝐂𝐨𝐫𝐞.𝐣𝐬 <erenxz01@gmail.com>
2026-02-28 18:37:35 -0400

    Actualizar update.js

--- commands/update.js
+++ commands/update.js
@@ -1,11 +1,12 @@
 case 'update': case 'up':
     // Obtener el número del sender sin el @s.whatsapp.net
     const senderNumber = sender.split('@')[0]
-    
+
     // Verificar si el número está en global.owner
     if (!global.owner.includes(senderNumber)) {
         return reply(`*🌴 Este Comandos No Esta Disponible En Mi Base De Datos*: *${command}*\n\n> _Te Recomiendo Usar *${usedPrefix}help* para ver los comandos disponibles._`)
-    
+    }
+
     await reply('🔄 Actualizando *Killua-Wa*')
     process.exit(0)
     break
